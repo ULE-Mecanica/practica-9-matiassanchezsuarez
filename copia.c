a544a1864706d320d6c2ad11716703de78d31596
matiassanchezsuarez <48126055+matiassanchezsuarez@users.noreply.github.com>
2019-04-05 12:38:29 +0200

    Practica cadenas

---
+++ copia.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i;
+	char cadena1[100],cadena2[100];
+	printf("Introduce la cadena 1");
+	fgets(cadena1,100,stdin);
+	printf("Introduce la cadena 2");
+	fgets(cadena2,100,stdin);
+ 	if(strlen(cadena1)==strlen(cadena2))
+	{
+			if (strcmp(cadena1,cadena2)==0)
+			{
+				printf("son iguales");
+			}
+			else
+			{
+				strcat(cadena1,cadena2);
+				printf("la cadena es: %c", cadena2);
+			}
+	}
+	else
+	{
+		strcpy(cadena1,cadena2);
+		printf("la cadena es: %c", cadena2);
+	}
+}
